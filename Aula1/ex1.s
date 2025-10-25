28d1e240f42d4f0058975515a80b642c622f4e74
Lazarosa04 <lazarosa@ua.pt>
2025-10-25 12:23:11 +0100

    aula1

---
+++ Aula1/ex1.s
@@ -0,0 +1,13 @@
+# Programa que implemente a expressão aritmética y = 2x + 8
+# x : $t0
+# resultado (y) : $t1
+
+	.data
+	.text
+	.globl main
+	
+main:	ori $t0,$0,3	#guardar em x ($t0) resultado da comparação de 3 com 0 ($0)
+	ori $t2,$0,8	#mesma coisa mas para guardarmos o 8 em $t2
+	add $t1,$t0,$t0	#guardar em y ($t1) soma de x consigo mesmo (2x)
+	add $t1,$t1,$t2 #somar a y o 8 que guardamos há bocado
+	jr $ra		#fim do programa
---
+++ Aula1/ex3.s
@@ -0,0 +1,28 @@
+# Programa que implemente a expressão aritmética y = 2x - 8
+# Usar system calls para pedir valor ao user
+# E para imprimir o resultado
+
+# x : $t0
+# resultado (y) : $t1
+
+	.data
+	# função .eqv permite guardar valores
+	.eqv printInt,1		#string "printInt" passa a ser equivalente a 1
+	.eqv readInt,5		#string "readInt" passa a ser equivalente a 5
+	
+	.text
+	.globl main
+	
+main:	li $v0,readInt		# operação li (load immediate) de readInt (5)
+	syscall			# chamada ao syscall
+				# resultado da syscall é guardado em $v0
+	move $t0,$v0		# mover o conteudo do resultado para $t0
+	li $t2,8		# load do 8 em $t2
+	add $t1,$t0,$t0		# guardar em y ($t1) a soma de x consigo mesmo (2x)
+	sub $t1,$t1,$t2		# subtrair a y ($t1) 8 ($t2)
+				# contas feitas, agora vamos dar print do resultado
+	li $v0,printInt		# função a chamar é a readInt($v0)
+	move $a0,$t1		# o valor a imprimir ($a0) é o y ($t1)
+	syscall			# chamada ao syscall
+	
+	jr $ra			#fim do programa
